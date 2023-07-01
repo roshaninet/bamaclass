0a5014472736a05f2ac3de87b0f8e510e260c155
Roshani <Roshani.net@gmail.com>
2023-07-02 02:49:49 +0330

    fx

--- components/Admin/Video/VideoCard.jsx
+++ components/Admin/Video/VideoCard.jsx
@@ -40,34 +40,20 @@ const VideoCard = ({loader, item, view, edit, remove}) => {
 
     return (
         <div className='w-100 shadow bg-white lh-lg position-relative rounded'>
-            <div className='row'>
-                <div className='col-4 align-self-center'>
-                    <div className={styles.videoImageContainer}>
-                        <ImageElement image={item.image || '/img/placeholder1.png'}
-                                      title={item.title}
-                                      className='w-100 bg-light rounded-top' width={240}
-                                      height={500} url={!!item.image} objectFit={'cover'}/>
+            <div className='w-100 px-3 py-2'>
+                <span className='d-block fw-bold'>{item.title}</span>
+
+                <div className='d-block font-13 mb-1'>
+                    <div className='d-inline-block ms-3'>کد ویدیو: <b>{item.id || "-"}</b></div>
+                    <div className='d-inline-block ms-3'>درس: <b>{item?.episode?.curse.title || "-"}</b></div>
+                    <div className='d-inline-block ms-3'>فصل: <b>{item?.episode?.curseIndex.title || "-"}</b>
                     </div>
                 </div>
-                <div className='col-7 align-self-center pe-0'>
-                    <div className='w-100 px-3 py-2'>
-                        <span className='d-block fw-bold'>{item.title}</span>
-
-                        <div className='d-block font-13 mb-1'>
-                            <div className='d-inline-block ms-3'>کد ویدیو: <b>{item.id || "-"}</b></div>
-                            <div className='d-inline-block ms-3'>درس: <b>{item?.episode?.curse.title || "-"}</b></div>
-                            <div className='d-inline-block ms-3'>فصل: <b>{item?.episode?.curseIndex.title || "-"}</b>
-                            </div>
-                        </div>
-                        <div className='d-block font-12'>
-                            <div
-                                className='d-inline-block ms-1 bg-dark rounded-pill text-light px-2'>{item.videoType === 1 ? 'لینکی' : 'پرلایک'}</div>
-                            <div
-                                className={`d-inline-block ms-1 rounded-pill text-light px-2 ${item.active ? 'bg-success' : 'bg-warning'}`}>{item.active ? 'فعال' : 'غیر فعال'}</div>
-                        </div>
-                    </div>
-
-
+                <div className='d-block font-12'>
+                    <div
+                        className='d-inline-block ms-1 bg-dark rounded-pill text-light px-2'>{item.videoType === 1 ? 'لینکی' : 'پرلایک'}</div>
+                    <div
+                        className={`d-inline-block ms-1 rounded-pill text-light px-2 ${item.active ? 'bg-success' : 'bg-warning'}`}>{item.active ? 'فعال' : 'غیر فعال'}</div>
                 </div>
             </div>
 
